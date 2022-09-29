f188e043bb50ac66fbe74f3e9c1daed3bebd0e05
fangzj1991 <48780090+fangzj1991@users.noreply.github.com>
2022-09-29 11:41:16 +0800

    热搜词修改，收藏可见站点名

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -936,7 +936,7 @@ public class LivePlayActivity extends BaseActivity {
         @Override
         public void run() {
             if (mVideoView == null) return;
-            tvNetSpeed.setText(String.format("%.2fMB/s", (float)mVideoView.getTcpSpeed() / 1024.0 / 1024.0));
+            tvNetSpeed.setText(String.format("%.2fMb/s", (float)mVideoView.getTcpSpeed() / 1024.0 / 1024.0));
             mHandler.postDelayed(this, 1000);
         }
     };
