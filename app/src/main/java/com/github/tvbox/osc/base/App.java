3779e130842acf4dea5fb2366acdcc2823b48d82
fangzj1991 <48780090+fangzj1991@users.noreply.github.com>
2022-11-09 14:35:54 +0800

    Update App.java
    revert back to ijk

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -51,7 +51,7 @@ public class App extends MultiDexApplication {
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
-            Hawk.put(HawkConfig.PLAY_TYPE, 2);
+            Hawk.put(HawkConfig.PLAY_TYPE, 1);
         }
         if (!Hawk.contains(HawkConfig.PLAY_RENDER)) {
             Hawk.put(HawkConfig.PLAY_RENDER, 1);
